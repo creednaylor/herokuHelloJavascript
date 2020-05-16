edb21e72d3009683569ebec46e5f3ba6ac807622
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-15 22:29:36 -0600

    2020-05-15 22:29, latest updates, using Python to commit

--- backend/startServer.js
+++ backend/startServer.js
@@ -1,38 +1,42 @@
-const importedExpress = require('express')
-const importedPath = require('path');
-const importedStringifyObject = require('stringify-object')
+const expressLibrary = require('express')
+const pathLibrary = require('path');
+const stringifyLibrary = require('stringify-object')
 // const importedUtil = require('util')
-const expressApp = importedExpress()
-const configFile = require('../aConfigFiles/config');
-const portStr = configFile.portStr;
+const expressApp = expressLibrary()
+const importedConfigFile = require('../aConfigFiles/config');
+const portStr = importedConfigFile.portStr;
 
+pathToThisJavascriptFileDirectory = __dirname
+pathToFrontEnd = pathLibrary.join(pathToThisJavascriptFileDirectory, '../frontend')
 
-expressApp.get('/canary', (reqObj, resObj) => {
-    resObj.send('The server is running...');
+expressApp.get('/canary', function(reqObj, resObj) {
+    
+    lengthOfStringToPrint = 10000;
 
+    reqObjStr = 'Printed reqObj: ' + stringifyLibrary(reqObj, {indent: ' ', singleQuotes: false}).substr(0, lengthOfStringToPrint)
+    resObjStr = 'Printed resObj: ' + stringifyLibrary(resObj, {indent: ' ', singleQuotes: false}).substr(0, lengthOfStringToPrint)
+    
     printingOn = false;
 
-    if (printingOn) {
-
-        lengthOfStringToPrint = 100;
-
-        console.log('Printed reqObj: ' + importedStringifyObject(reqObj, {
-            indent: '  ',
-            singleQuotes: false
-        }).substr(0, lengthOfStringToPrint));
-    
-    
-        console.log('Printed resObj: ' + importedStringifyObject(resObj, {
-            indent: '  ',
-            singleQuotes: false
-        }).substr(0, lengthOfStringToPrint));
+    if (printingOn) {    
+        console.log(reqObjStr + resObjStr)
     }
+
+    resObj.send('The server is running...')
+
+})
+
+expressApp.get('/cat', function(reqObj, resObj) {
+
+    resObj.json({
+        'cat eyes': 'yellow',
+        'collar': 'red'
+    });
         
 })
 
 
-pathToThisJavascriptFileDirectory = __dirname
-expressApp.use('/', importedExpress.static(importedPath.join(pathToThisJavascriptFileDirectory, '../frontend')))
 
+expressApp.use('/', expressLibrary.static(pathToFrontEnd))
 
 expressApp.listen(portStr, () => console.log(`Example app listening at http://localhost:${portStr}`))
